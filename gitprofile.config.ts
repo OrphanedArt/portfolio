41d3e42536f00ade914babce004dc2c160302d89
Kholid Shohibul Fathoni <fathoni.info@gmail.com>
2026-02-23 10:06:47 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -62,7 +62,7 @@ const CONFIG = {
             'Creates, schedules, and analyzes content across platforms to boost brand awareness, engagement, and sales for CV. Lentera Fajar',
           imageUrl:
             'https://fathoni.co.uk/wp-content/uploads/2026/02/IMG_20260223_092740.jpg',
-          link: 'https://wwww.facebook.com/CV.LENTERAFAJA',
+          link: 'https://wwww.facebook.com/CV.LENTERAFAJAR',
         },     
       ],
     },
